04c6826290676c88d797124ac484835e1d65d7b3
KariukiAbel <abelmuriithi@gmail.com>
2019-09-28 12:38:40 +0300

    Getting even and odd indexed characters from multiple strings

--- src/com/nabesh/Solution1.java
+++ src/com/nabesh/Solution1.java
@@ -1,4 +1,30 @@
 package com.nabesh;
 
+import java.util.Scanner;
+
 public class Solution1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char [] even = new char[];
+        String []s = new String[n];
+        for (int i = 0; i < n; i++) {
+            s [i]= sc.next();
+        }
+        sc.close();
+        for (int j=0; j<s.length; j++){
+            String word = s[j];
+            char [] ch = word.toCharArray();
+            for (int k = 0; k < ch.length; k++) {
+                if (k % 2 == 0){
+                    System.out.print(ch[k] );
+                    even[k] = ch[k];
+                }
+                else {
+                    System.out.print(" "+ch[k] );
+                }
+            }
+        }
+    }
 }
